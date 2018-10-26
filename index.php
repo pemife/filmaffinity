7e03b0c4de65893702ae4c87ceb5cdc43278d411
Ricardo Pérez <ricpelo@gmail.com>
2018-10-26 20:53:14 +0200

    Primera versión

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Bases de datos</title>
+    </head>
+    <body>
+        <?php
+        $pdo = new PDO('pgsql:host=localhost;dbname=fa', 'fa', 'fa');
+        $st = $pdo->query('SELECT * FROM generos');
+        $res = $st->fetchAll();
+        ?>
+        <table>
+            <thead>
+                <th>Id</th>
+                <th>Género</th>
+            </thead>
+            <tbody>
+                <?php foreach ($res as $fila): ?>
+                    <tr>
+                        <td><?= $fila['id'] ?></td>
+                        <td><?= $fila['genero'] ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </body>
+</html>
